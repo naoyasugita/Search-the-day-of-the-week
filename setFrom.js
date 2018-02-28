4958f6da04c675260dd68b0f7d5a13aff93924e0
n_sugita <sugita6878@gmail.com>
2018-02-28 18:38:04 +0900

    fill in date

---
+++ setFrom.js
@@ -0,0 +1,18 @@
+(function() {
+  var optionLoop,this_year, today;
+  today = new Date();
+  this_year = today.getFullYear();
+
+  optionLoop = function(start, end, id) {
+    var i, opt;
+
+    opt = '';
+    for (i = start; i <= end ; i++) {
+        opt += "<option value='" + i + "'>" + i + "</option>";
+    }
+    return document.getElementById(id).innerHTML = opt;
+  };
+  optionLoop(1950, this_year, 'id_year');
+  optionLoop(1, 12, 'id_month');
+  optionLoop(1, 31, 'id_day');
+})();
